a165064afe24ee4d80aa199d7977604d2940017a
joaohupalo <joaoghupalo@gmail.com>
2025-10-30 21:54:07 -0300

    Aula 30/10

--- the-moviebox-react/src/App.tsx
+++ the-moviebox-react/src/App.tsx
@@ -1,12 +1,17 @@
 import { Header } from "./components/layout/header"
 import { Footer } from "./components/layout/footer"
+import { Navbar } from "./components/layout/navbar"
+import { MovieContent } from "./cases/movies/components/movie-content"
 
 function App() {
 
   return (
     <>
     <Header/>
-    <main></main>
+    <main>
+      <Navbar/>
+      <MovieContent/>
+    </main>
     <Footer/>
     </>
   )
